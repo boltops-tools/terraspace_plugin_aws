f8f99342223385dbf0985d080618a439dd8d9e51
Tung Nguyen <tongueroo@gmail.com>
2020-05-26 03:24:42 +0000

    fix specs

--- spec/terraspace_provider_spec.rb
+++ spec/terraspace_provider_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe TerraspaceProvider do
   it "has a version number" do
     expect(TerraspaceProvider::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
