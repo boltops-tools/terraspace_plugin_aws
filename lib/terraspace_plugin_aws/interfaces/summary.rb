1164cd1d617ce739202f5830a4854be3fc3be313
Tung Nguyen <tongueroo@gmail.com>
2020-07-20 00:21:17 +0000

    implement delete_empty_statefile

--- lib/terraspace_plugin_aws/interfaces/summary.rb
+++ lib/terraspace_plugin_aws/interfaces/summary.rb
@@ -16,5 +16,45 @@ module TerraspacePluginAws::Interfaces
         )
       end
     end
+
+    # interface method
+    def delete_empty_statefile(key)
+      delete_lock_id(key)
+      delete_s3_file(key)
+    end
+
+  private
+    def delete_s3_file(key)
+      s3.delete_object(
+        bucket: @bucket,
+        key: key,
+      )
+      # resp is:
+      #
+      #   <struct Aws::S3::Types::DeleteObjectOutput
+      #    delete_marker=nil,
+      #    version_id=nil,
+      #    request_charged=nil>
+    end
+
+    def delete_lock_id(key)
+      lock_id = "#{@bucket}/#{key}-md5"
+      table_name = @info['dynamodb_table']
+      dynamodb.delete_item(
+        key: {LockID: lock_id},
+        table_name: table_name,
+      )
+      # resp is:
+      #
+      #   #<struct Aws::DynamoDB::Types::DeleteItemOutput
+      #    attributes=nil,
+      #    consumed_capacity=nil,
+      #    item_collection_metrics=nil>
+    rescue Aws::DynamoDB::Errors::ResourceNotFoundException => e
+      # Exception happens when dynamodb table doesnt exist
+      # If the lock item is missing, it successfully sends the api call to delete, even though there's nothing to delete
+      logger.error "ERROR: #{e.class}: #{e.message}"
+      logger.error "Table may not exist"
+    end
   end
 end
