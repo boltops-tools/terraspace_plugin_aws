93777588d906efe19f0ebfdbc73b725cb43d330a
Tung Nguyen <tongueroo@gmail.com>
2021-12-14 17:28:56 +0000

    use region configured in the backend.tf for the s3 client

--- lib/terraspace_plugin_aws/clients.rb
+++ lib/terraspace_plugin_aws/clients.rb
@@ -8,23 +8,32 @@ module TerraspacePluginAws
     extend Memoist
 
     def s3
-      Aws::S3::Client.new
+      Aws::S3::Client.new(client_options)
     end
     memoize :s3
 
     def secretsmanager
-      Aws::SecretsManager::Client.new
+      Aws::SecretsManager::Client.new(client_options)
     end
     memoize :secretsmanager
 
     def ssm
-      Aws::SSM::Client.new
+      Aws::SSM::Client.new(client_options)
     end
     memoize :ssm
 
     def dynamodb
-      Aws::DynamoDB::Client.new
+      Aws::DynamoDB::Client.new(client_options)
     end
     memoize :dynamodb
+
+    # Typically inferred from AWS_REGION unless set in the backend.tf
+    def client_options
+      if @info['region']
+        {region: @info['region']}
+      else
+        {}
+      end
+    end
   end
 end
