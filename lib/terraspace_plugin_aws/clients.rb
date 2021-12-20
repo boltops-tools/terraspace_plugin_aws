136651a5d818ac3bb604a9bc824126aba1120c30
raymond.yu <raymond.yu@plexure.com>
2021-12-20 22:28:57 +1300

    Add a check to see if @info is null before creating client

--- lib/terraspace_plugin_aws/clients.rb
+++ lib/terraspace_plugin_aws/clients.rb
@@ -9,22 +9,38 @@ module TerraspacePluginAws
     include Options
 
     def s3
-      Aws::S3::Client.new(client_options)
+      if @info
+        Aws::S3::Client.new(client_options)
+      else
+        Aws::S3::Client.new
+      end
     end
     memoize :s3
 
     def secretsmanager
-      Aws::SecretsManager::Client.new(client_options)
+      if @info
+        Aws::SecretsManager::Client.new(client_options)
+      else
+        Aws::SecretsManager::Client.new
+      end
     end
     memoize :secretsmanager
 
     def ssm
-      Aws::SSM::Client.new(client_options)
+      if @info
+        Aws::SSM::Client.new(client_options)
+      else
+        Aws::SSM::Client.new
+      end
     end
     memoize :ssm
 
     def dynamodb
-      Aws::DynamoDB::Client.new(client_options)
+      if @info
+        Aws::DynamoDB::Client.new(client_options)
+      else
+        Aws::DynamoDB::Client.new
+      end
     end
     memoize :dynamodb
   end
